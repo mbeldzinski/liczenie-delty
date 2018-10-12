d468f629db9b7e1f9f345465f4e4170bc18d9b13
Marcin <marcin.beldzinski@gmail.com>
2018-10-12 18:06:10 +0200

    dodalem funkcje

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+void wczytaj_parametry(int *a, int *b, int *c);
+
+void Brak_rozwiazan();
+
+void rowna_zeru(double x1);
+
+void wieksza_od_zera(double x1, double x2);
+
 int main() {
     int a;
     int b;
@@ -10,12 +18,7 @@ int main() {
     double delta;
 
 
-    printf("Podaj wartość a:");
-    scanf("%i", &a);
-    printf("Podaj wartość b:");
-    scanf("%i", &b);
-    printf("Podaj wartość c:");
-    scanf("%i", &c);
+    wczytaj_parametry(&a, &b, &c);
 
 
 //D = b 2 - 4 * a* c
@@ -25,16 +28,31 @@ int main() {
     printf("Delta= %1lf\n", delta);
 
     if (delta < 0) {
-        printf("Delta nie ma rozwiązań");
+        Brak_rozwiazan();
     }
     if (delta == 0) {
         x1 = -b / (2 * a);
-        printf("Rozwiązanie %1f", x1);
+        rowna_zeru(x1);
     }
     if (delta > 0) {
         x1 = (-b - sqrt(delta)) / 2 * a;
         x2 = (-b + sqrt(delta)) / 2 * a;
-        printf("Rozwiązanie x1=%1lf,x2=%lf\n", x1, x2);
+        wieksza_od_zera(x1, x2);
     }
     return 0;
+}
+
+void wieksza_od_zera(double x1, double x2) { printf("Rozwiązanie x1=%1lf,x2=%lf\n", x1, x2); }
+
+void rowna_zeru(double x1) { printf("Rozwiązanie %1f", x1); }
+
+void Brak_rozwiazan() { printf("Delta nie ma rozwiązań"); }
+
+void wczytaj_parametry(int *a, int *b, int *c) {
+    printf("Podaj wartość a:");
+    scanf("%i", a);
+    printf("Podaj wartość b:");
+    scanf("%i", b);
+    printf("Podaj wartość c:");
+    scanf("%i", c);
 }
